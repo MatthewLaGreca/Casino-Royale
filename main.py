8b7d72481553a7dff18fc10b702959c16a0e6857
YungButtCoin <yungz0rk@gmail.com>
2024-03-30 13:34:40 -0500

    Modularized code used in Ultimate Texas Hold Em, so that it can be used in games

--- main.py
+++ main.py
@@ -52,6 +52,10 @@ class Deck():
 
     def shuffle_deck(self):
         shuffle(self.cards)
+    
+    # the function to deal an individual card
+    def deal(self):
+        return self.cards.pop(-1)
 
 # Poker Hand Ranks
 # Royal Flush: A straight from Ace to 10, all of the same suit
@@ -63,37 +67,85 @@ class Deck():
 # Trips: 3 cards of the same type, with the other two being different from the 3 of the same and each other (AAAKQ)
 # Pair: 2 cards of the same type, with the other three being different from the 2 of the same and each other (AAKQJ)
 # High Card: 5 cards that aren't of the same suit, sequential order and there are no dupes (AKQJ9)
-def valid_wager(wager, table_min, table_max, bet_name):
-    while (wager < table_min) or (wager > table_max):
+        
+def wager_prompt(bet_name, table_min, table_max, bonus = False):
+    if bonus:
+        return int(input(f"How much would you like to put on the {bet_name}? $0 to ${table_max} (must be increments of $5): "))
+    else:
+        return int(input(f"How much would you like to put on the {bet_name}? ${table_min} to ${table_max}: "))  
+
+# Condensed both wager validation functions into one
+    
+# def valid_wager(bet_name, wager, table_min, table_max):
+#     while (wager < table_min) or (wager > table_max):
+#         if wager < table_min:
+#             print(f'You can not bet less than the table minimum of ${table_min}')
+#             wager = wager_prompt(bet_name, table_max, table_max)
+#         elif wager > table_max:
+#             print(f'You can not bet more than the table maximum of ${table_max}')
+#             wager = wager_prompt(bet_name, table_max, table_max)
+#     return wager
+
+# def valid_side(bet_name, wager, table_max):
+#     while (wager < 0) or (wager > table_max) or (wager % 5 != 0):
+#         if wager < 0:
+#             print(f'You can not bet less than $0')
+#             wager = wager_prompt(bet_name, table_max, True)
+#         elif wager > table_max:
+#             print(f'You can not bet more than the {bet_name} maximum of ${table_max}')
+#             wager = wager_prompt('Trips', 100, 0, True)
+#         elif wager % 5 != 0:
+#             print(f'Your {bet_name} bet has to be an increment of $5')
+#             wager = wager_prompt('Trips', 100, 0, True)
+#     return wager
+       
+def valid_wager(bet_name, wager, table_min, table_max, bonus = False):
+    while (wager < table_min) or (wager > table_max) or ((wager % 5 != 0) and (bonus == True)):
         if wager < table_min:
-            print('You can not bet less than the table minimum of $', table_min, sep='')
-            wager = int(input(f"How much would you like to put on the {bet_name}? ${table_min} to ${table_max}: "))
+            print(f'You can not bet less than the table minimum of ${table_min}')
+            wager = wager_prompt(bet_name, table_min, table_max, bonus)
         elif wager > table_max:
-            print('You can not bet more than the table maximum of $', table_max, sep='')
-            wager = int(input(f"How much would you like to put on the {bet_name}? ${table_min} to ${table_max}: "))
+            print(f'You can not bet more than the table maximum of ${table_max}')
+            wager = wager_prompt(bet_name, table_min, table_max, bonus)
+        elif wager % 5 != 0:
+            print(f'Your {bet_name} bet has to be an increment of $5')
+            wager = wager_prompt(bet_name, table_max, table_max, bonus)
     return wager
 
+def playing_bonus(bonus_name):
+    decision = input(f"Would you like to play {bonus_name}? (Yes/No): ") #Still needs to be programmed for error handling
+    if decision:
+        bonus_wager = wager_prompt(bonus_name, 0, 100, True)
+        confirmed_bonus = valid_wager(bonus_name, bonus_wager, 0, 100, True)
+        print(f'You decide to place ${confirmed_bonus} on {bonus_name}')
+        return confirmed_bonus
+    else:
+        print(f'You decide to forego playing {bonus_name}')
+        return 0
 
 #The portion of the program that is Ultimate Texas Hold Em     
 def uth(table_min):
+
+    deck = Deck()
+    main_bet = 'Ante'
+    side_bet = 'Trips'
+    final_bet = 'Raise'
 
     #Only want to display this message if changing from another casino game to UTH, or starting up playing UTH
     # if not player.currently_playing:
     #     print('You approach a table of familiar faces, only to be greeted with a table minimum of $', table_min, ' and a table max of $100\n')
     #     print('Debbie: "Hey there, hun!  Stay a while and make your self comfortable!"')
-    wager = int(input(f"How much would you like to put on the Ante? ${table_min} to $100: "))
-    confirmed_wager = valid_wager(wager, table_min, 100, 'Ante')
-    # while (wager < table_min) or (wager > 100):
-    #     if wager < table_min:
-    #         print('You can not bet less than the table minimum of $', table_min)
-    #         wager = int(input(f"How much would you like to put on the Ante? ${table_min} to $100: "))
-    #     elif wager > 100:
-    #         print ('You can not bet more than the table maximum of $100')
-    #         wager = int(input(f"How much would you like to put on the Ante? ${table_min} to $100: "))
+
+    #Getting the Ante and Blind from the player
+    wager = wager_prompt(main_bet, table_min, 100)
+    confirmed_wager = valid_wager(main_bet, wager, table_min, 100)
     print("You put $", confirmed_wager, " on the Ante and the Blind, placing a total wager of $", 2*confirmed_wager, sep='')
-    playing_trips = input("Would you like to play trips? (Yes/No): ")
-    if playing_trips:
-        trips_wager = int(input(f"How much would you like to put on the Trips? $5 to $100 (must be increments of $5): "))
+
+    #Getting the Trips side bet from the player
+    side_wager = playing_bonus(side_bet)
+
+    #Wagers are placed, we now deal the cards
+    print('Best of luck to you!')
     
 
 
